b756078a6abce7479459eee4029622ddb43daa0f
gene-network <58380257+gene-network@users.noreply.github.com>
2019-12-01 14:10:36 +0900

    Add files via upload

---
+++ Step4. Hub shared plot/Hub_Duplicate.R
@@ -0,0 +1,421 @@
+####highsort high
+rm(list=ls())
+#library(readxl)
+library(network)
+library(sqldf)
+
+name_index <- c( "supp 1" )
+#normal_lambda <- c( 0.31431 )
+
+
+high_list <- as.data.frame(read.csv("file path"))
+info_du <- as.data.frame(read.csv("file path"))
+
+file_name=paste("file path")
+result_table<-as.matrix(read.table(file_name, header=FALSE, sep=",")) 
+file_name=paste("file path")
+hub_gene <- as.data.frame(read.table(file_name, header=TRUE, sep=","))
+
+hub_gene$degree <- as.numeric(hub_gene$degree)
+hub_gene <- hub_gene[c(order(-hub_gene$degree)),]
+rownames(hub_gene) <- NULL
+View(hub_gene[1:15,1])
+
+gene_top <- hub_gene[10,1] #top 10
+#gene_top
+for(i in gene_top){
+  #i
+  result_table_c <- result_table[result_table[,1] %in% i | result_table[,2] %in% i,]  
+  #result_table_c
+  b <- as.vector(unique(rbind(as.matrix(unique(result_table_c[,1])), as.matrix(unique(result_table_c[,2])))))
+  c <- length(b) 
+  true_c<-matrix(rep(0, c*c), c, c)
+  
+  colnames(true_c) <- b
+  rownames(true_c) <- b
+  
+  node_info <- as.data.frame(b)
+  colnames(node_info) <- c("name")
+  node_info <- sqldf('SELECT a.name, b.type
+                     FROM node_info a
+                     LEFT JOIN high_list b USING(name)')
+  
+  #edge_info_un <- as.data.frame(b)
+  #colnames(edge_info_un) <- c("name")
+  #edge_info_un <- sqldf('SELECT c.name, d.type 
+  #FROM edge_info_un c 
+  #LEFT JOIN info_un d USING(name)')
+  
+  edge_info_du <- as.data.frame(b)
+  colnames(edge_info_du) <- c("name")
+  edge_info_du <- sqldf('SELECT e.name, f.type 
+                        FROM edge_info_du e 
+                        LEFT JOIN info_du f USING(name)')
+  
+  for(j in 1:dim(result_table_c)[1])
+  {
+    true_c[result_table_c[j,1], result_table_c[j,2]] <- result_table_c[j,3]
+  }
+  
+  true_X<-true_c
+  true_X[true_X!=0]<-1
+  
+  d<-as.numeric(result_table_c[,3])
+  
+  
+  aa1 <- network(true_X)
+  
+  
+  file_name=paste("file path",i,".png", sep="")
+  png(filename=file_name, height=1000, width=1000, bg="white")
+  plot(aa1,displaylabels=TRUE,
+       boxed.labels=FALSE,
+       arrowhead.cex=0,
+       #label.cex= ifelse(is.na(edge_info_du$type),1.3,
+       #ifelse(edge_info_du$type=="b",4,1.3)), #character expansion factor for label text,
+       #label.lwd = ifelse(edge_info_du$type == "b", 10 , 0),
+       pad=0.25,
+       label.pos=10,
+       edge.lwd=d*3.5,
+       vertex.cex=2.5,
+       #vertex.cex= ifelse(is.na(edge_info_un$type) ,2.5,
+       #ifelse(edge_info_un$type == "a",2.5,2.5)), #expansion factor for vertices
+       #edge.col = ifelse(is.na(edge_info_du$type),"black",
+       #ifelse(edge_info_du$type=="b","dark red","black")),
+       label.col = ifelse(is.na(edge_info_du$type),"black",
+                          ifelse(edge_info_du$type == "b","dark red","black")),
+       #vertex.enclose = ifelse(is.na(edge_info_un$type),FALSE,
+       #ifelse(edge_info_un$type=="a",TRUE,FALSE)),
+       vertex.col =  ifelse(is.na(node_info$type),"white",
+                            ifelse(node_info$type == "up", "light pink", "light green")),
+       
+       mode ="fruchtermanreingold",  # "kamadakawai",  #"circle", #"fruchtermanreingold", 
+       cex.main=3)
+  
+  dev.off()
+}
+
+######highsort low
+rm(list=ls())
+#library(readxl)
+library(network)
+library(sqldf)
+
+low <- read.csv("file path")
+high <- read.csv("file path")
+
+ID.low <- as.character(low[,1])
+ID.high <-as.character(high[,1])
+
+match.index <- match(ID.high, ID.low)
+#match.index1 <- match(ID.low,ID.high)
+
+low_data <- low[match.index,]
+#high_data <- high[match.index1,]
+
+file_name_combined_data =paste("file path", sep="")
+write.table(low_data ,row.names=FALSE,col.names=TRUE, file=file_name_combined_data ,sep=",")
+
+
+
+
+name_index <- c( "supp 1" )
+#normal_lambda <- c( 0.31431 )
+
+
+high_list <- as.data.frame(read.csv("file path"))
+info_du <- as.data.frame(read.csv("file path"))
+
+file_name=paste("file path")
+result_table<-as.matrix(read.table(file_name, header=FALSE, sep=",")) 
+file_name=paste("file path")
+hub_gene <- as.data.frame(read.table(file_name, header=TRUE, sep=","))
+
+hub_gene$degree <- as.numeric(hub_gene$degree)
+#hub_gene <- hub_gene[c(order(-hub_gene$degree)),]
+rownames(hub_gene) <- NULL
+View(hub_gene[1:15,1])
+gene_top <- hub_gene[10,1] #top 10
+#gene_top
+for(i in gene_top){
+  #i
+  result_table_c <- result_table[result_table[,1] %in% i | result_table[,2] %in% i,]  
+  #result_table_c
+  b <- as.vector(unique(rbind(as.matrix(unique(result_table_c[,1])), as.matrix(unique(result_table_c[,2])))))
+  c <- length(b) 
+  true_c<-matrix(rep(0, c*c), c, c)
+  
+  colnames(true_c) <- b
+  rownames(true_c) <- b
+  
+  node_info <- as.data.frame(b)
+  colnames(node_info) <- c("name")
+  node_info <- sqldf('SELECT a.name, b.type
+                     FROM node_info a
+                     LEFT JOIN high_list b USING(name)')
+  
+  #edge_info_un <- as.data.frame(b)
+  #colnames(edge_info_un) <- c("name")
+  #edge_info_un <- sqldf('SELECT c.name, d.type 
+  #FROM edge_info_un c 
+  #LEFT JOIN info_un d USING(name)')
+  
+  edge_info_du <- as.data.frame(b)
+  colnames(edge_info_du) <- c("name")
+  edge_info_du <- sqldf('SELECT e.name, f.type 
+                        FROM edge_info_du e 
+                        LEFT JOIN info_du f USING(name)')
+  
+  for(j in 1:dim(result_table_c)[1])
+  {
+    true_c[result_table_c[j,1], result_table_c[j,2]] <- result_table_c[j,3]
+  }
+  
+  true_X<-true_c
+  true_X[true_X!=0]<-1
+  
+  d<-as.numeric(result_table_c[,3])
+  
+  
+  aa1 <- network(true_X)
+  
+  
+  file_name=paste("file path",i,".png", sep="")
+  png(filename=file_name, height=1000, width=1000, bg="white")
+  plot(aa1,displaylabels=TRUE,
+       boxed.labels=FALSE,
+       arrowhead.cex=0,
+       #label.cex= ifelse(is.na(edge_info_du$type),1.3,
+       #ifelse(edge_info_du$type=="b",4,1.3)), #character expansion factor for label text,
+       #label.lwd = ifelse(edge_info_du$type == "b", 10 , 0),
+       pad=0.25,
+       label.pos=10,
+       edge.lwd=d*3.5,
+       vertex.cex=2.5,
+       #vertex.cex= ifelse(is.na(edge_info_un$type) ,2.5,
+       #ifelse(edge_info_un$type == "a",2.5,2.5)), #expansion factor for vertices
+       #edge.col = ifelse(is.na(edge_info_du$type),"black",
+       #ifelse(edge_info_du$type=="b","dark red","black")),
+       label.col = ifelse(is.na(edge_info_du$type),"black",
+                          ifelse(edge_info_du$type == "b","dark red","black")),
+       #vertex.enclose = ifelse(is.na(edge_info_un$type),FALSE,
+       #ifelse(edge_info_un$type=="a",TRUE,FALSE)),
+       vertex.col =  ifelse(is.na(node_info$type),"white",
+                            ifelse(node_info$type == "up", "light green", "light pink")),
+       
+       mode ="fruchtermanreingold",  # "kamadakawai",  #"circle", #"fruchtermanreingold", 
+       cex.main=3)
+  
+  dev.off()
+}
+
+######negasort nega
+rm(list=ls())
+#library(readxl)
+library(network)
+library(sqldf)
+
+name_index <- c( "supp 1" )
+#normal_lambda <- c( 0.31431 )
+
+
+high_list <- as.data.frame(read.csv("file path"))
+info_du <- as.data.frame(read.csv("file path"))
+
+file_name=paste("file path")
+result_table<-as.matrix(read.table(file_name, header=FALSE, sep=",")) 
+file_name=paste("file path")
+hub_gene <- as.data.frame(read.table(file_name, header=TRUE, sep=","))
+
+hub_gene$degree <- as.numeric(hub_gene$degree)
+hub_gene <- hub_gene[c(order(-hub_gene$degree)),]
+rownames(hub_gene) <- NULL
+View(hub_gene[1:15,1])
+gene_top <- hub_gene[10,1] #top 10
+#gene_top
+for(i in gene_top){
+  #i
+  result_table_c <- result_table[result_table[,1] %in% i | result_table[,2] %in% i,]  
+  #result_table_c
+  b <- as.vector(unique(rbind(as.matrix(unique(result_table_c[,1])), as.matrix(unique(result_table_c[,2])))))
+  c <- length(b) 
+  true_c<-matrix(rep(0, c*c), c, c)
+  
+  colnames(true_c) <- b
+  rownames(true_c) <- b
+  
+  node_info <- as.data.frame(b)
+  colnames(node_info) <- c("name")
+  node_info <- sqldf('SELECT a.name, b.type
+                     FROM node_info a
+                     LEFT JOIN high_list b USING(name)')
+  
+  #edge_info_un <- as.data.frame(b)
+  #colnames(edge_info_un) <- c("name")
+  #edge_info_un <- sqldf('SELECT c.name, d.type 
+  #FROM edge_info_un c 
+  #LEFT JOIN info_un d USING(name)')
+  
+  edge_info_du <- as.data.frame(b)
+  colnames(edge_info_du) <- c("name")
+  edge_info_du <- sqldf('SELECT e.name, f.type 
+                        FROM edge_info_du e 
+                        LEFT JOIN info_du f USING(name)')
+  
+  for(j in 1:dim(result_table_c)[1])
+  {
+    true_c[result_table_c[j,1], result_table_c[j,2]] <- result_table_c[j,3]
+  }
+  
+  true_X<-true_c
+  true_X[true_X!=0]<-1
+  
+  d<-as.numeric(result_table_c[,3])
+  
+  
+  aa1 <- network(true_X)
+  
+  
+  file_name=paste("file path",i,".png", sep="")
+  png(filename=file_name, height=1000, width=1000, bg="white")
+  plot(aa1,displaylabels=TRUE,
+       boxed.labels=FALSE,
+       arrowhead.cex=0,
+       #label.cex= ifelse(is.na(edge_info_du$type),1.3,
+       #ifelse(edge_info_du$type=="b",4,1.3)), #character expansion factor for label text,
+       #label.lwd = ifelse(edge_info_du$type == "b", 10 , 0),
+       pad=0.25,
+       label.pos=10,
+       edge.lwd=d*3.5,
+       vertex.cex=2.5,
+       #vertex.cex= ifelse(is.na(edge_info_un$type) ,2.5,
+       #ifelse(edge_info_un$type == "a",2.5,2.5)), #expansion factor for vertices
+       #edge.col = ifelse(is.na(edge_info_du$type),"black",
+       #ifelse(edge_info_du$type=="b","dark red","black")),
+       label.col = ifelse(is.na(edge_info_du$type),"black",
+                          ifelse(edge_info_du$type == "b","dark red","black")),
+       #vertex.enclose = ifelse(is.na(edge_info_un$type),FALSE,
+       #ifelse(edge_info_un$type=="a",TRUE,FALSE)),
+       vertex.col =  ifelse(is.na(node_info$type),"white",
+                            ifelse(node_info$type == "up", "light green", "light pink")),
+       
+       mode ="fruchtermanreingold",  # "kamadakawai",  #"circle", #"fruchtermanreingold", 
+       cex.main=3)
+  
+  dev.off()
+}
+#########negasort high
+
+rm(list=ls())
+#library(readxl)
+library(network)
+library(sqldf)
+
+
+low <- read.csv("file path")
+high <- read.csv("file path")
+
+ID.low <- as.character(low[,1])
+ID.high <-as.character(high[,1])
+
+#match.index <- match(ID.high, ID.low)
+match.index1 <- match(ID.low,ID.high)
+
+#low_data <- low[match.index,]
+high_data <- high[match.index1,]
+
+
+file_name_combined_data =paste("file path", sep="")
+write.table(high_data ,row.names=FALSE,col.names=TRUE, file=file_name_combined_data ,sep=",")
+
+
+name_index <- c( "supp 1" )
+#normal_lambda <- c( 0.31431 )
+
+
+high_list <- as.data.frame(read.csv("file path"))
+info_du <- as.data.frame(read.csv("file path"))
+
+file_name=paste("file path")
+result_table<-as.matrix(read.table(file_name, header=FALSE, sep=",")) 
+file_name=paste("file path")
+hub_gene <- as.data.frame(read.table(file_name, header=TRUE, sep=","))
+
+hub_gene$degree <- as.numeric(hub_gene$degree)
+#hub_gene <- hub_gene[c(order(-hub_gene$degree)),]
+rownames(hub_gene) <- NULL
+View(hub_gene[1:15,1])
+gene_top <- hub_gene[10,1] #top 10
+#gene_top
+for(i in gene_top){
+  #i
+  result_table_c <- result_table[result_table[,1] %in% i | result_table[,2] %in% i,]  
+  #result_table_c
+  b <- as.vector(unique(rbind(as.matrix(unique(result_table_c[,1])), as.matrix(unique(result_table_c[,2])))))
+  c <- length(b) 
+  true_c<-matrix(rep(0, c*c), c, c)
+  
+  colnames(true_c) <- b
+  rownames(true_c) <- b
+  
+  node_info <- as.data.frame(b)
+  colnames(node_info) <- c("name")
+  node_info <- sqldf('SELECT a.name, b.type
+                     FROM node_info a
+                     LEFT JOIN high_list b USING(name)')
+  
+  #edge_info_un <- as.data.frame(b)
+  #colnames(edge_info_un) <- c("name")
+  #edge_info_un <- sqldf('SELECT c.name, d.type 
+  #FROM edge_info_un c 
+  #LEFT JOIN info_un d USING(name)')
+  
+  edge_info_du <- as.data.frame(b)
+  colnames(edge_info_du) <- c("name")
+  edge_info_du <- sqldf('SELECT e.name, f.type 
+                        FROM edge_info_du e 
+                        LEFT JOIN info_du f USING(name)')
+  
+  for(j in 1:dim(result_table_c)[1])
+  {
+    true_c[result_table_c[j,1], result_table_c[j,2]] <- result_table_c[j,3]
+  }
+  
+  true_X<-true_c
+  true_X[true_X!=0]<-1
+  
+  d<-as.numeric(result_table_c[,3])
+  
+  
+  aa1 <- network(true_X)
+  
+  
+  file_name=paste("file path",i,".png", sep="")
+  png(filename=file_name, height=1000, width=1000, bg="white")
+  plot(aa1,displaylabels=TRUE,
+       boxed.labels=FALSE,
+       arrowhead.cex=0,
+       #label.cex= ifelse(is.na(edge_info_du$type),1.3,
+       #ifelse(edge_info_du$type=="b",4,1.3)), #character expansion factor for label text,
+       #label.lwd = ifelse(edge_info_du$type == "b", 10 , 0),
+       pad=0.25,
+       label.pos=10,
+       edge.lwd=d*3.5,
+       vertex.cex=2.5,
+       #vertex.cex= ifelse(is.na(edge_info_un$type) ,2.5,
+       #ifelse(edge_info_un$type == "a",2.5,2.5)), #expansion factor for vertices
+       #edge.col = ifelse(is.na(edge_info_du$type),"black",
+       #ifelse(edge_info_du$type=="b","dark red","black")),
+       label.col = ifelse(is.na(edge_info_du$type),"black",
+                          ifelse(edge_info_du$type == "b","dark red","black")),
+       #vertex.enclose = ifelse(is.na(edge_info_un$type),FALSE,
+       #ifelse(edge_info_un$type=="a",TRUE,FALSE)),
+       vertex.col =  ifelse(is.na(node_info$type),"white",
+                            ifelse(node_info$type == "up", "light pink", "light green")),
+       
+       mode ="fruchtermanreingold",  # "kamadakawai",  #"circle", #"fruchtermanreingold", 
+       cex.main=3)
+  
+  dev.off()
+}
+
